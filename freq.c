59518e5654f9464d568c16ffb14b56dec9b63be4
Delpreti <rddesiqueira@gmail.com>
2019-06-30 22:35:54 -0300

    Melhorias no codigo

--- freq.c
+++ freq.c
@@ -17,14 +17,10 @@ int main(int argc, char *argv[]) {
 		freq[w] = 0;
 	}
 
-    // Le a mensagem inserida e guarda no vetor a frequencia dos caracteres
+    // Guarda no vetor a frequencia dos caracteres
     for (int i = 1; argv[i] != 0; i++) {
 		for (int j = 0; argv[i][j] != '\0'; j++) {
-			for (int k = 0; k < SP; k++) { // Possivelmente redundante fazer um for aqui
-				if (argv[i][j] == asc[k]) {
-					freq[k] += 1;
-				}
-			}
+			freq[(int)argv[i][j]] += 1;
 		}
 	}
 
